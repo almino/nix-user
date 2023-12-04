e6ab15a732ea39e1eba7db7739ba474e9e9e27ef
Almino Melo <almino.melo@gmail.com>
2023-12-03 21:28:35 -0300

    Pra poder esconder, tem que ser assim

--- hidden-user.nix
+++ hidden-user.nix
@@ -12,4 +12,14 @@
       IncludeAll = lib.mkDefault false;
     };
   };
+
+  users.users.almino = {
+    createHome = lib.mkDefault true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    group = lib.mkDefault "users";
+    home = lib.mkDefault "/home/almino";
+    initialPassword = lib.mkDefault "123456";
+    isNormalUser = lib.mkForce false;
+    isSystemUser = lib.mkDefault true;
+  };
 }
