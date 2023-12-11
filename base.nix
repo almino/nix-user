f317ae53037a39ef3998edd9f41a433c41649ca5
Almino Melo <almino.melo@gmail.com>
2023-12-11 13:26:11 -0300

    Coisa de código e VS Code

--- base.nix
+++ base.nix
@@ -6,6 +6,13 @@
     now = "date +\"%Y-%m-%d--%H-%M-%S\"";
   };
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    nil
+    nixpkgs-fmt
+  ];
+
   imports = [ ./fish.nix ./typography.nix ];
 
   users.users.almino.packages = with pkgs; [
