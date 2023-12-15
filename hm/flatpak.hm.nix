639f2a89d836c8bf9d9880c4f60a0548eec63707
Almino Melo <almino.melo@gmail.com>
2023-12-15 16:44:47 -0300

    Esse comando não deve ser executado aqui

--- hm/flatpak.hm.nix
+++ hm/flatpak.hm.nix
@@ -1,9 +1,8 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 
 {
   home.activation = {
-    flatpak = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      # ln -s /run/current-system/sw/share/X11/fonts "$(xdg-user-dir HOME)/.local/share/fonts"
+    flatpak = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       $DRY_RUN_CMD /run/current-system/sw/bin/flatpak --user override --filesystem=/run/current-system/sw/share/X11/fonts
       $DRY_RUN_CMD /run/current-system/sw/bin/flatpak --user override --filesystem=$HOME/.icons
     '';
