5bcdb6129be9ed03e89ba494cabb603d41ce5929
Almino Melo <almino.melo@gmail.com>
2024-03-07 11:53:21 -0300

    Starship por padrão!

---
+++ fish.nix
@@ -0,0 +1,65 @@
+{ lib, pkgs, ... }:
+
+{
+  # https://nixos.wiki/wiki/Fish#Installation
+  programs.fish.enable = true;
+  programs.fish.shellAbbrs =
+    let
+      backup-dir = "${tarxz} --file ../(${now}).(basename $PWD).tar.xz (/run/current-system/sw/bin/ls -A $PWD)";
+      gc = "sudo nix-collect-garbage";
+      hms = "home-manager switch --no-out-link -b (date +\"%Y%m%d-%H%M%S\")";
+      noBuild = "--no-build-nix";
+      now = "date +\"%Y-%m-%d--%H-%M-%S\"";
+      pip = "sudo python3 -m pip";
+      rebuild = "sudo nixos-rebuild";
+      restart = "sudo systemctl kexec";
+      screen-backup-dir = "screen sh -c \"${backup-dir}\"";
+      tarxz = "tar --checkpoint=1500 --create --xz";
+      up = "--upgrade-all";
+    in
+    {
+      backup-dir = lib.mkDefault backup-dir;
+      bd = lib.mkDefault backup-dir;
+      full-switch = lib.mkDefault "${rebuild} switch ${noBuild}";
+      gc = lib.mkDefault gc;
+      hms = lib.mkDefault hms;
+      nboot = lib.mkDefault "${rebuild} boot; and ${gc}";
+      nreboot = lib.mkDefault "${rebuild} boot ${noBuild}; and ${gc}; and ${restart}";
+      nswitch = lib.mkDefault "${rebuild} switch ${noBuild}";
+      ntest = lib.mkDefault "${rebuild} test ${noBuild}";
+      pcp = lib.mkDefault "rsync -ah --progress";
+      pip = lib.mkDefault pip;
+      pip3 = lib.mkDefault pip;
+      reboot = lib.mkDefault restart;
+      restart = lib.mkDefault restart;
+      rmf = lib.mkDefault "rm --force --recursive";
+      rpull = "git pull --recurse-submodules";
+      rup = lib.mkDefault (builtins.toString [
+        "${rebuild} boot ${noBuild} ${up};"
+        "and ${gc};"
+        "and ${restart}"
+      ]);
+      sbackup-dir = lib.mkDefault screen-backup-dir;
+      sbd = lib.mkDefault screen-backup-dir;
+      srmf = lib.mkDefault "sudo rm --interactive=once --recursive";
+      stash = lib.mkDefault "git stash -u -m \"\"";
+      targit = lib.mkDefault "tar --checkpoint=5 --create --xz --exclude-from=.gitignore --file";
+      tarignore = lib.mkDefault "tar --checkpoint=5 --create --xz --exclude-from=.tarignore --file";
+      tarxz = lib.mkDefault "tar --checkpoint=5 --create --xz --file";
+      up = lib.mkDefault "${rebuild} switch ${noBuild} ${up}";
+    };
+
+  programs.starship.enable = true;
+
+  # Somente meu usuário
+  users.users.almino.packages = with pkgs; [
+    fish
+    fishPlugins.colored-man-pages
+    fishPlugins.fzf-fish # Ctrl + Alt + {P,S,L,F}
+    fishPlugins.grc # colors
+    fishPlugins.pisces # VS-Code-like terminal
+    fishPlugins.puffer # .. !! !$
+    fzf
+    grc
+  ];
+}
