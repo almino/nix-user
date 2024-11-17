2f559627814c77952a1ad96b941934537ab33cfb
Almino Melo <almino.melo@gmail.com>
2024-11-17 15:08:13 -0300

    Tava dando problema!

--- typography.nix
+++ typography.nix
@@ -4,9 +4,9 @@
   # !! NÃO altere as linhas ACIMA !!
 
   # https://nixos.wiki/wiki/Fonts#Installing_fonts_on_NixOS
-  fonts.packages = with pkgs; [
-    nerdfonts
-  ];
+  # fonts.packages = with pkgs; [
+  #   nerdfonts
+  # ];
 
   # https://nixos.wiki/wiki/Fonts#:~:text=fonts%5B2%5D-,Flatpak%20applications%20can%27t%20find%20system%20fonts,-Enable%20fontDir%20in
   fonts.fontDir.enable = true;
