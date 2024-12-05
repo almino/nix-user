3bfe2161a7db590118b0f90c7290c007fa3b5842
Almino Melo <almino.melo@gmail.com>
2024-12-05 12:09:01 -0300

    Extensão para o NextCloud

--- gnome.nix
+++ gnome.nix
@@ -6,6 +6,7 @@
     gnome.gnome-tweaks
     gnomeExtensions.advanced-alttab-window-switcher
     gnomeExtensions.media-controls
+    gnomeExtensions.nextcloud-folder
     gnomeExtensions.pano
     gnomeExtensions.shutdowntimer # https://extensions.gnome.org/extension/4372/shutdowntimer/
   ];
