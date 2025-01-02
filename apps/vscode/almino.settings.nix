c68b54982333bd7d55597323e46417e72e9b8b8a
Almino Melo <almino.melo@gmail.com>
2025-01-02 12:39:46 -0300

    Pra usar o desktop de casa via SSH

--- apps/vscode/almino.settings.nix
+++ apps/vscode/almino.settings.nix
@@ -2,4 +2,8 @@
   "explorer.confirmDelete" = false;
   # "remote.SSH.configFile" = "\${userHome}/.ssh/config";
   "remote.SSH.configFile" = ".ssh/config";
+  "remote.SSH.remotePlatform" = {
+    desktop = "linux";
+    hp = "linux";
+  };
 }
--- apps/vscode/settings.nix
+++ apps/vscode/settings.nix
@@ -86,7 +86,6 @@
     [ "nixpkgs-fmt" ];
   "prettier.printWidth" = 50;
   "redhat.telemetry.enabled" = true;
-  "remote.SSH.remotePlatform".hp = "linux";
   "remote.SSH.useLocalServer" = false;
   "sql-formatter.uppercase" = true;
   "terminal.integrated.fontFamily" = lib.mkDefault
