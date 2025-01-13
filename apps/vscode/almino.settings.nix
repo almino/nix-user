302b47f97a8a965a4cb459f42743c29467e978cc
Almino Melo <almino.melo@gmail.com>
2025-01-13 14:43:43 -0300

    Pra usar no artemis

--- apps/vscode/almino.settings.nix
+++ apps/vscode/almino.settings.nix
@@ -3,6 +3,7 @@
   # "remote.SSH.configFile" = "\${userHome}/.ssh/config";
   "remote.SSH.configFile" = ".ssh/config";
   "remote.SSH.remotePlatform" = {
+    artemis = "linux";
     desktop = "linux";
     hp = "linux";
   };
