91547cda6bb6ebcc15ce85fe973c805fd913e08b
Almino Melo <almino.melo@gmail.com>
2025-04-08 11:16:07 -0300

    Mapas mentais e diagramas!

--- default.nix
+++ default.nix
@@ -8,6 +8,8 @@
     # Os aplicativos abaixo estão disponíveis
     # apenas para este usuário
     packages = with pkgs; [
+      dia
+      drawio
       # librewolf # muito fresco pra configurações
       minder
       nextcloud-client
