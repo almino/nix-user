50bd4596dccedec7e9762c5997f99dc6665d3447
Almino Melo <almino.melo@gmail.com>
2025-10-19 13:07:14 -0300

    Testando MCP's

--- apps/vscode/almino.settings.nix
+++ apps/vscode/almino.settings.nix
@@ -1,6 +1,7 @@
 {
   "chat.mcp.access" = "all";
   "chat.mcp.discovery.enabled" = true;
+  "chat.mcp.gallery.enabled" = true;
   "explorer.confirmDelete" = false;
   # "remote.SSH.configFile" = "\${userHome}/.ssh/config";
   "remote.SSH.configFile" = ".ssh/config";
