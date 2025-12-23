06215d0ac0d572a39322528471afbfa39408756b
Almino Melo <almino.melo@gmail.com>
2025-12-23 18:18:27 -0300

    Um monte de coisa nova que nem sei se vou usar

--- gnome.nix
+++ gnome.nix
@@ -4,14 +4,26 @@
   imports = [ ./apps/gnome.default-applications.nix ];
 
   users.users.almino.packages = with pkgs; [
+    citations # Manage your bibliographies using the BibTeX format
+    collision # Check hashes for your files
+    curtail # Simple & useful image compressor
     endeavour
+    errands # Manage your tasks
+    file-roller # GUI for tar zip rar
     gnome-decoder
     gnome-frog
     gnome-pomodoro
+    gnome-solanum # pomodoro
+    gnomeExtensions.bing-wallpaper-changer
     gnomeExtensions.browser-search-provider
     gnomeExtensions.firefox-profiles
     gnomeExtensions.one-thing
     gnomeExtensions.pop-shell
-    gnomeExtensions.bing-wallpaper-changer
+    junction # Choose the application to open files and links
+    lorem # Generate placeholder text
+    peazip
+    switcheroo # App for converting images between different formats
+    textpieces # Swiss knife of text processing
+    tuba # Browse the Fediverse
   ];
 }
