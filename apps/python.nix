c856c238026af2ddf161d473a775d19fa82360f5
Almino Melo <almino.melo@gmail.com>
2026-01-01 11:44:43 -0300

    Coisas do Python relacionadas a gŕaficos

--- apps/python.nix
+++ apps/python.nix
@@ -3,6 +3,7 @@
 {
   users.users.almino.packages = with pkgs; [
     (python3.withPackages (python-pkgs: with python-pkgs; [
+      altair # visualização de dados
       beautifulsoup4
       black
       # pdf2docx # não funciona
@@ -14,6 +15,9 @@
       python-frontmatter
       python-slugify
       regex
+      ruff
+      seaborn
+      vl-convert-python
       werkzeug
     ]))
     ruff
