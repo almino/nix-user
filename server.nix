0789c8655ee6da4cd7ad8c4bda579bab57f5e9d7
Almino Melo <almino.melo@gmail.com>
2026-03-29 11:22:41 -0300

    Úteis num servidor

--- server.nix
+++ server.nix
@@ -1,6 +1,14 @@
 { lib, pkgs, ... }:
 
 {
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # eza # base.nix
+    # htop # base.nix
+    lsof
+  ];
+
   imports = [ ./base.nix ./www-data.nix ];
 
   # Para usar o VS Code via SSH
@@ -14,6 +22,7 @@
   users.users.almino = {
     isNormalUser = lib.mkDefault true;
     packages = with pkgs; [
+      bat
       uv # MCP NixOS no VS Code
     ];
   };
